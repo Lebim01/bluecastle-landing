954d933fcd157f11aa8d0a9b5d80e935f247d66f
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-09-17 14:06:54 -0600

    metadata

--- src/utilities/generateMeta.ts
+++ src/utilities/generateMeta.ts
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 
-import type { Media, Page, Post, Config } from '../payload-types'
+import type { Media, Page, Config } from '../payload-types'
 
 import { mergeOpenGraph } from './mergeOpenGraph'
 import { getServerSideURL } from './getURL'
@@ -27,8 +27,8 @@ export const generateMeta = async (args: {
   const ogImage = getImageURL(doc?.meta?.image)
 
   const title = doc?.meta?.title
-    ? doc?.meta?.title + ' | Payload Website Template'
-    : 'Payload Website Template'
+    ? doc?.meta?.title + ' | Bluecastle'
+    : 'Blue Castle Club | Exclusive Wealth & Lifestyle Membership'
 
   return {
     description: doc?.meta?.description,
